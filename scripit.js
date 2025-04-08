eff37c6e994f16a5d4840946467f4d9239fd677b
dhessaa <andressasouza017@gmail.com>
2025-04-08 20:23:34 -0300

    Add files via upload

---
+++ scripit.js
@@ -0,0 +1,25 @@
+var diaSemana = Number(prompt("digite um dia da semana de 1 a 7"))
+
+switch(diaSemana){
+    case 1: 
+        alert("Domingo")
+        break;
+    case 2: 
+        alert("Segunda")
+        break;
+    case 3:
+         alert("terça")
+         break;
+    case 4:
+        alert("quarta")
+        break;
+    case 5:
+        alert('quinta')
+        break;
+    case 6:
+        alert ("sexta")
+        break;
+    case 7:
+        alert ("sabado")
+        break;
+}
